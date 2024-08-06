f5643b178bbe185b0176b4ff55c70cfc30e1f669
Sundanpatyad <sundansharma600@gmail.com>
2024-08-06 18:37:21 +0530

    put

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -3,7 +3,8 @@ import Template from "../components/core/Auth/Template"
 function Login() {
   return (
     <Template
-      title=" Hey 👋 Welcome Back"
+      title=" Hey 👋"
+      title1="Welcome Back"
       description1="Build skills for today, tomorrow, and beyond."
       description2="Education to future-proof your career."
       formType="login"
