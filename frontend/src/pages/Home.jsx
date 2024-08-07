99bd1efc7c2fb324eee0da979aa2a130a438a91a
Sundanpatyad <sundansharma600@gmail.com>
2024-08-07 19:33:42 +0530

    push

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -230,7 +230,7 @@ const Home = () => {
         <div className="absolute cursor-pointer inset-0 w-full h-full bg-black [mask-image:radial-gradient(ellipse_at_center,transparent_10%,black)]"></div>
 
           <PlaceholdersAndVanishInputDemo/>
-        <Link to={"/mocktest"} className="bg-slate-900 no-underline group cursor-pointer relative shadow-2xl shadow-zinc-900 rounded-full p-px text-xs font-semibold leading-6  text-white inline-block">
+        <Link to={"/mocktest"} className="bg-zinc-900 no-underline group cursor-pointer relative shadow-2xl shadow-zinc-900 rounded-full p-px text-xs font-semibold leading-6  text-white inline-block">
           <span className="absolute inset-0 overflow-hidden rounded-full">
             <span className="absolute inset-0 rounded-full bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(56,189,248,0.6)_0%,rgba(56,189,248,0)_75%)] opacity-0 transition-opacity duration-500 group-hover:opacity-100" />
           </span>
