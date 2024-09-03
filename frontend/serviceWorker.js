9161aed743caf8f7fe25a2c69da43433678995b8
Sundanpatyad <sundansharma600@gmail.com>
2024-09-03 21:36:22 +0530

    push acode

--- frontend/serviceWorker.js
+++ frontend/serviceWorker.js
@@ -70,3 +70,15 @@ self.addEventListener('message', (event) => {
 });
 
 // Any other custom service worker logic can go here.
+
+self.addEventListener('install', function(event) {
+  console.log('Service Worker installing.');
+});
+
+self.addEventListener('activate', function(event) {
+  console.log('Service Worker activating.');
+});
+
+self.addEventListener('fetch', function(event) {
+  console.log('Fetching:', event.request.url);
+});
