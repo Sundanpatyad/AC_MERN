315877c22c4e2c1cd37d14eb7e2b55cb2d9bbd94
sundansharmaprimotech <sundan.primotech@gmail.com>
2024-09-04 19:14:30 +0530

    push code

--- frontend/src/components/ui/InitialLoader.jsx
+++ frontend/src/components/ui/InitialLoader.jsx
@@ -26,7 +26,7 @@ const YourComponent = () => {
         </div>
       </div>
       <motion.div
-        className="text-zinc-400 text-center text-base mt-20"
+        className="text-zinc-400 text-center text-base"
         initial={{ opacity: 0, y: 50 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 1, ease: "easeOut" }}
