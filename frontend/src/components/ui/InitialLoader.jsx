5ee2c2fd4166f7dd14fda549ebaf177c4f44a111
sundansharmaprimotech <sundan.primotech@gmail.com>
2024-09-04 15:17:01 +0530

    push

--- frontend/src/components/ui/InitialLoader.jsx
+++ frontend/src/components/ui/InitialLoader.jsx
@@ -4,7 +4,7 @@ import "./design.css";
 
 const YourComponent = () => {
   return (
-    <div className="h-screen">
+    <div className="h-screen overflow-hidden">
       <div className="content w-[100vw] h-[80vh] p-10 flex flex-row align-center justify-center">
         <div className="flex justify-center">
           <div className="A-del1 del"></div>
