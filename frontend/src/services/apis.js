8f4a59af34107911449d322ba615debf4b7e8c0e
sundansharmaprimotech <sundan.primotech@gmail.com>
2024-11-14 18:25:24 +0530

    dev

--- frontend/src/services/apis.js
+++ frontend/src/services/apis.js
@@ -1,7 +1,8 @@
 
 // const BASE_URL = 'https://awakening-classes.up.railway.app';
 // const BASE_URL = 'http://localhost:8000';
-const BASE_URL = 'https://goldfish-app-xyu28.ondigitalocean.app';
+// const BASE_URL = 'https://goldfish-app-xyu28.ondigitalocean.app';
+const BASE_URL = 'https://clownfish-app-5bzlf.ondigitalocean.app';  /// dev Url 
 
 export const studyMaterialEndPoints = {
     FETCH_EXAMS : `${BASE_URL}/api/v1/materials/getExam`,
